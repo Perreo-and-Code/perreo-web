a748f8530186a7ce69cae4a5e70e946257e90acb
stevenbrand99 <stevenbrand99>
2022-02-24 23:43:34 -0500

    in progress website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./Perreo&code.2.gif";
+import instagram from "./instagram.png";
+import twitter from "./twitter.png";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Brand gay
-        </p>
+        <small>Página en construcción...</small>
+        <div className="App-link-container">
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/perreoandcode/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={instagram} alt="instagram" className="App-instagram" />
         </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/perreoandcode"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitter} alt="twitter" className="App-twitter" />
+        </a>
+        </div>
       </header>
     </div>
   );
